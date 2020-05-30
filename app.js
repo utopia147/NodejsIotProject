f6e9ac1f5dc7ecf655ce47dc187b499635217a7a
utopia <wertata147@gmail.com>
2020-05-30 13:36:07 +0700

    del comment app.js

--- app.js
+++ app.js
@@ -18,46 +18,17 @@ app.get("/", (req, res) => {
 var usersRouter = require('./routes/usersrouter')
 var channelRouter = require('./routes/channelrouter')
 var nodemcuRouter = require('./routes/nodemcurouter')
+
 app.use('/api/users', usersRouter)
 app.use('/api/channel', channelRouter)
 app.use('/api/nodemcu'.nodemcuRouter)
+
 app.use((req, res, next) => {
   var err = new Error("ไม่พบ path ที่คุณต้องการ");
   err.status = 404;
   next(err);
 });
 
-
-
-
-// //settime
-// app.post("/settime/add", (req, res) => {
-//   settimeModel.create(req.body, (err, doc) => {
-//     if (err) res.json({ result: "Qurry failed" })
-//     res.json({
-//       result: "Querry success",
-//       channelid: req.body.channelid,
-//     })
-//   })
-// })
-
-// //item
-// app.post('/item/add', (req, res) => {
-//   itemModel.create(req.body, (err, doc) => {
-//     if (err) res.json({ result: "Qurry failed" })
-//     res.json({
-//       result: "Querry success",
-//       channelid: req.body.channelid,
-//     })
-//   })
-// })
-//
-// app.put("/", (req, res) => {
-//   res.send("Status:" + res.statusCode + " Message: put ");
-// });
-// app.delete("/", (req, res) => {
-//   res.send("Status:" + res.statusCode + " Message: delete");
-// });
 
 app.listen(port, () => {
   console.log("App running htpp://localhost:" + port)
